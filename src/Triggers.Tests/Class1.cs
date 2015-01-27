cc5b75584ca81b5226d8f53cd7352f431b100456
Lucas Schad <schad.lucas@gmail.com>
2015-01-27 14:02:56 +0100

    TeamCity NUnit Test Commit

---
+++ src/Triggers.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace Triggers.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TeamCity_Test()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [Test]
+        public void TeamCity_Test_Fails()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
