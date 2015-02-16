d662bc46191bc565f9e392490948f44bdbff225b
Lucas Schad <schad.lucas@gmail.com>
2015-02-16 04:37:43 +0100

    Defined LogMessage model

--- src/Triggers.UI/js/model.js
+++ src/Triggers.UI/js/model.js
@@ -2,3 +2,11 @@ Triggers.Notification = DS.Model.extend({
     caption: DS.attr('string'),
     message: DS.attr('string')
 });
+
+Triggers.Log = DS.Model.extend({
+    component: DS.attr('string'),
+    caption: DS.attr('string'),
+    message: DS.attr('string'),
+    level: DS.attr('string'),
+    timestamp: DS.attr('date')
+});
