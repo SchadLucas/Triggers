829d2ea1d237d385b49a7c24297c92dcbaf1394e
Lucas Schad <schad.lucas@gmail.com>
2015-02-16 23:37:05 +0100

    Add dummy action

--- src/Triggers.UI/js/controller.js
+++ src/Triggers.UI/js/controller.js
@@ -1,15 +1,18 @@
-Triggers.NotificationsController = Ember.Controller.extend({
-
-});
+Triggers.NotificationsController = Ember.Controller.extend({});
 
 Triggers.LogController = Ember.Controller.extend({
-    processedData: function (){
+    processedData: function () {
         result = [];
-        this.model.forEach(function(e){
+        this.model.forEach(function (e) {
             var x = e;
             x.set('level', 'loglevel loglevel-' + e.get('level').toLowerCase());
             result.push(x);
         });
         return result;
-    }.property('model')
+    }.property('model'),
+    actions: {
+        clearLogHistory: function () {
+            console.warn('FUNCTION NOT IMLEMENTED');
+        }
+    }
 });
