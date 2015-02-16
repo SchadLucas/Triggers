a8a5e69211c1ee92771215149f54bf981b14242a
Lucas Schad <schad.lucas@gmail.com>
2015-02-16 06:08:48 +0100

    Added LogLevel converter

--- src/Triggers.UI/js/controller.js
+++ src/Triggers.UI/js/controller.js
@@ -1,3 +1,15 @@
 Triggers.NotificationsController = Ember.Controller.extend({
 
 });
+
+Triggers.LogController = Ember.Controller.extend({
+    processedData: function (){
+        result = [];
+        this.model.forEach(function(e){
+            var x = e;
+            x.set('level', 'loglevel-' + e.get('level').toLowerCase());
+            result.push(x);
+        });
+        return result;
+    }.property('model')
+});
